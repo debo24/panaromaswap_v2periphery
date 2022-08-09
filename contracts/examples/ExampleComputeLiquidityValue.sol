94893c9298b91a951bcc7474353ea6c421605c90
debo24 <debobrata.k@digixhub.com>
2022-08-09 19:30:15 +0530

    FC

---
+++ contracts/examples/ExampleComputeLiquidityValue.sol
@@ -0,0 +1,90 @@
+pragma solidity =0.6.6;
+
+import '../libraries/PanaromaswapV2LiquidityMathLibrary.sol';
+
+contract ExampleComputeLiquidityValue {
+    using SafeMath for uint256;
+
+    address public immutable factory;
+
+    constructor(address factory_) public {
+        factory = factory_;
+    }
+
+    // see PanaromaswapV2LiquidityMathLibrary#getReservesAfterArbitrage
+    function getReservesAfterArbitrage(
+        address tokenA,
+        address tokenB,
+        uint256 truePriceTokenA,
+        uint256 truePriceTokenB
+    ) external view returns (uint256 reserveA, uint256 reserveB) {
+        return PanaromaswapV2LiquidityMathLibrary.getReservesAfterArbitrage(
+            factory,
+            tokenA,
+            tokenB,
+            truePriceTokenA,
+            truePriceTokenB
+        );
+    }
+
+    // see PanaromaswapV2LiquidityMathLibrary#getLiquidityValue
+    function getLiquidityValue(
+        address tokenA,
+        address tokenB,
+        uint256 liquidityAmount
+    ) external view returns (
+        uint256 tokenAAmount,
+        uint256 tokenBAmount
+    ) {
+        return PanaromaswapV2LiquidityMathLibrary.getLiquidityValue(
+            factory,
+            tokenA,
+            tokenB,
+            liquidityAmount
+        );
+    }
+
+    // see PanaromaswapV2LiquidityMathLibrary#getLiquidityValueAfterArbitrageToPrice
+    function getLiquidityValueAfterArbitrageToPrice(
+        address tokenA,
+        address tokenB,
+        uint256 truePriceTokenA,
+        uint256 truePriceTokenB,
+        uint256 liquidityAmount
+    ) external view returns (
+        uint256 tokenAAmount,
+        uint256 tokenBAmount
+    ) {
+        return PanaromaswapV2LiquidityMathLibrary.getLiquidityValueAfterArbitrageToPrice(
+            factory,
+            tokenA,
+            tokenB,
+            truePriceTokenA,
+            truePriceTokenB,
+            liquidityAmount
+        );
+    }
+
+    // test function to measure the gas cost of the above function
+    function getGasCostOfGetLiquidityValueAfterArbitrageToPrice(
+        address tokenA,
+        address tokenB,
+        uint256 truePriceTokenA,
+        uint256 truePriceTokenB,
+        uint256 liquidityAmount
+    ) external view returns (
+        uint256
+    ) {
+        uint gasBefore = gasleft();
+        PanaromaswapV2LiquidityMathLibrary.getLiquidityValueAfterArbitrageToPrice(
+            factory,
+            tokenA,
+            tokenB,
+            truePriceTokenA,
+            truePriceTokenB,
+            liquidityAmount
+        );
+        uint gasAfter = gasleft();
+        return gasBefore - gasAfter;
+    }
+}
